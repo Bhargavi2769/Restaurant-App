b14c074530e0f65834c2d080f10a5d4aa2a1799a
Bhargavi2769 <62629611+Bhargavi2769@users.noreply.github.com>
2024-03-05 15:29:35 +0530

    Update index.js

--- src/components/AppItem/index.js
+++ src/components/AppItem/index.js
@@ -2,15 +2,43 @@
 import './index.css'
 
 const AppItem = props => {
-  const {displayData} = props
-  const {dishName, dishImage} = displayData
+  const {dishDetails} = props
+  const {
+    dishName,
+    dishImage,
+    dishCurrency,
+    dishPrice,
+    dishDescription,
+    dishCalories,
+  } = dishDetails
+
+  const c = 0
 
   return (
     <li className="app-item">
+      <div>
+        <p className="app-name">{dishName}</p>
+        <p>
+          {dishCurrency} {dishPrice}
+        </p>
+        <p>{dishDescription}</p>
+        <div className="button-custom">
+          <button type="button" className="button-class">
+            -
+          </button>
+          <p className="button-text">{c}</p>
+          <button type="button" className="button-class">
+            +
+          </button>
+        </div>
+      </div>
+      <div>
+        <p className="claories-text">{dishCalories}claories</p>
+      </div>
       <img className="app-image" src={dishImage} alt={dishName} />
-      <p className="app-name">{dishName}</p>
     </li>
   )
 }
 
 export default AppItem
+
